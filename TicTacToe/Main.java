1f0523b109b45ada45090db506f7b9eea8063883
Sadiq R <113649841+Airemz@users.noreply.github.com>
2023-05-09 18:07:38 -0400

    Add files via upload

---
+++ TicTacToe/Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println();
+    System.out.println("       x           x");
+    System.out.println("       x           x");
+    System.out.println("       x           x");
+    System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    System.out.println("x   x  x   O O O   x");
+    System.out.println("  x    x   O   O   x");
+    System.out.println("x   x  x   O O O   x");
+    System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    System.out.println("       x           x");
+    System.out.println("       x           x");
+    System.out.println("       x           x");
+
+  }
+}
